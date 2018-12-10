01240762b619a48f2e87a2c68161134e99007334
abbhughjo <43879922+abbhughjo@users.noreply.github.com>
2018-12-10 09:13:23 +0100

    mikrodator
    mikrodator kod

---
+++ mikrodator/ljussensor.ino
@@ -0,0 +1,22 @@
+#define D7 13
+
+int AI_LightSens = 0; 
+
+int LightValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(AI_LightSens, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+
+void loop() {
+  LightValue = analogRead(AI_LightSens);
+  Serial.println(LightValue);
+  delay(500);
+  if(LightValue > 10)
+    digitalWrite(13, HIGH);
+  else
+    digitalWrite(13, LOW);
+}
